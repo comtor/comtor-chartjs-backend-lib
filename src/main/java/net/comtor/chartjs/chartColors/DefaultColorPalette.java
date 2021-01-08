0fe5281af6bcbf215c91b6e3b7848a0d077a1723
Brayan Ariza <brayan@comtor.net>
2021-01-08 09:39:21 -0500

    Colores

--- src/main/java/net/comtor/chartjs/chartColors/DefaultColorPalette.java
+++ src/main/java/net/comtor/chartjs/chartColors/DefaultColorPalette.java
@@ -8,35 +8,35 @@ import java.util.Random;
  */
 public class DefaultColorPalette extends ChartColorPalette{
 
-    public DefaultColorPalette() {
+        public DefaultColorPalette() {
 
-        this.addColor(new ChartColor(1f, 0f, 0f));      // Red      #FF0000
-        this.addColor(new ChartColor(0f, 0f, 1f));      // Blue     #0000FF
-        this.addColor(new ChartColor(0f, 1f, 0f));      // Green    #00FF00
-        this.addColor(new ChartColor(1f, 1f, 0f));      // Yellow   #FFFF00
-        this.addColor(new ChartColor(0f, 1f, 1f));      // Cyan     #00FFFF
-        this.addColor(new ChartColor(1f, 0f, 1f));      // Magenta  #FF00FF
+        this.addColor(new ChartColor(1*255, 0*255, 0*255));      // Red      #FF0000
+        this.addColor(new ChartColor(0*255, 0*255, 1*255));      // Blue     #0000FF
+        this.addColor(new ChartColor(0*255, 1*255, 0*255));      // Green    #00FF00
+        this.addColor(new ChartColor(1*255, 1*255, 0*255));      // Yellow   #FFFF00
+        this.addColor(new ChartColor(0*255, 1*255, 1*255));      // Cyan     #00FFFF
+        this.addColor(new ChartColor(1*255, 0*255, 1*255));      // Magenta  #FF00FF
         
-        this.addColor(new ChartColor(1f, 0.5f, 0f));            // #FF7F00
-        this.addColor(new ChartColor(0.5f, 1f, 0f));            // #7FFF00
-        this.addColor(new ChartColor(0f, 0.5f, 1f));            // #007FFF
-        this.addColor(new ChartColor(1f, 0f, 0.5f));            // #FF007F
-        this.addColor(new ChartColor(0.5f, 0f, 1f));            // #7F00FF
-        this.addColor(new ChartColor(0.5f, 0.5f, 0.5f));        // #7F7F7F  
+        this.addColor(new ChartColor(1*255, 0.5*255, 0*255));            // #FF7F00
+        this.addColor(new ChartColor(0.5*255, 1*255, 0*255));            // #7FFF00
+        this.addColor(new ChartColor(0*255, 0.5*255, 1*255));            // #007FFF
+        this.addColor(new ChartColor(1*255, 0*255, 0.5*255));            // #FF007F
+        this.addColor(new ChartColor(0.5*255, 0*255, 1*255));            // #7F00FF
+        this.addColor(new ChartColor(0.5*255, 0.5*255, 0.5*255));        // #7F7F7F  
 
-        this.addColor(new ChartColor(0.8f, 0f, 0f));
-        this.addColor(new ChartColor(0f, 0f, 0.8f));
-        this.addColor(new ChartColor(0f, 0.8f, 0f));
-        this.addColor(new ChartColor(0.8f, 0.8f, 0f));
-        this.addColor(new ChartColor(0f, 0.8f, 0.8f));
-        this.addColor(new ChartColor(0.8f, 0f, 0.8f));
+        this.addColor(new ChartColor(0.8*255, 0*255, 0*255));
+        this.addColor(new ChartColor(0*255, 0*255, 0.8*255));
+        this.addColor(new ChartColor(0*255, 0.8*255, 0*255));
+        this.addColor(new ChartColor(0.8*255, 0.8*255, 0*255));
+        this.addColor(new ChartColor(0*255, 0.8*255, 0.8*255));
+        this.addColor(new ChartColor(0.8*255, 0*255, 0.8*255));
 
-        this.addColor(new ChartColor(0.8f, 0.4f, 0f));
-        this.addColor(new ChartColor(0.4f, 0.8f, 0f));
-        this.addColor(new ChartColor(0f, 0.4f, 0.8f));
-        this.addColor(new ChartColor(0.8f, 0f, 0.4f));
-        this.addColor(new ChartColor(0.4f, 0f, 0.8f));
-        this.addColor(new ChartColor(0.4f, 0.4f, 0.4f));
+        this.addColor(new ChartColor(0.8*255, 0.4*255, 0*255));
+        this.addColor(new ChartColor(0.4*255, 0.8*255, 0*255));
+        this.addColor(new ChartColor(0*255, 0.4*255, 0.8*255));
+        this.addColor(new ChartColor(0.8*255, 0*255, 0.4*255));
+        this.addColor(new ChartColor(0.4*255, 0*255, 0.8*255));
+        this.addColor(new ChartColor(0.4*255, 0.4*255, 0.4*255));
     }
         
     public ChartColor getRandomColor(){
